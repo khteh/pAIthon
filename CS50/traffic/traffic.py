b3a01459ff1648a69eba4686b78affaf5fc5b14a
Kok How, Teh <funcoolgeek@gmail.com>
2024-12-16 15:49:58 +0800

    Experiment with softmax activator and add layer

--- CS50/traffic/traffic.py
+++ CS50/traffic/traffic.py
@@ -96,10 +96,12 @@ def get_model():
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
     model = models.Sequential()
-    model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)))
+    model.add(layers.Conv2D(64, (3, 3), activation='softmax', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(64, activation='softmax'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(64, activation='softmax'))
     model.add(layers.Dropout(0.5))
     model.add(layers.Dense(NUM_CATEGORIES))
     model.compile(optimizer='adam',
