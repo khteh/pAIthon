cec11a58c17193f5a6d1916b6c76b9e6c2a5645c
Kok How, Teh <funcoolgeek@gmail.com>
2025-01-03 18:08:50 +0800

    Save and using saved model

--- CS50/traffic/traffic.py
+++ CS50/traffic/traffic.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import os
+from pathlib import Path
 import sys
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
@@ -16,13 +17,12 @@ IMG_HEIGHT = 30
 NUM_CATEGORIES = 43
 TEST_SIZE = 0.4
 
-
 def main():
 
     # Check command-line arguments
     if len(sys.argv) not in [2, 3]:
         sys.exit("Usage: python traffic.py data_directory [model.h5]")
-
+    print(f"TF v{tf.version.VERSION}")
     # Get image arrays and labels for all image files
     images, labels = load_data(sys.argv[1])
 
@@ -31,22 +31,28 @@ def main():
     x_train, x_test, y_train, y_test = train_test_split(
         np.array(images), np.array(labels), test_size=TEST_SIZE
     )
+    saveModel: bool = False
+    if (len(sys.argv) == 3) and sys.argv[2] and Path(sys.argv[2]).exists() and Path(sys.argv[2]).is_file():
+        print(f"Using saved model {sys.argv[2]}...")
+        model = tf.keras.models.load_model(sys.argv[2])
+    else:
+        # Get a compiled neural network
+        model = get_model()
+        # Fit model on training data
+        model.fit(x_train, y_train, epochs=EPOCHS)
+        saveModel = True
 
-    # Get a compiled neural network
-    model = get_model()
-
-    # Fit model on training data
-    model.fit(x_train, y_train, epochs=EPOCHS)
+    # Display the model's architecture
+    model.summary()
 
     # Evaluate neural network performance
     model.evaluate(x_test,  y_test, verbose=2)
 
     # Save model to file
-    if len(sys.argv) == 3:
+    if saveModel and len(sys.argv) == 3:
         filename = sys.argv[2]
         model.save(filename)
         print(f"Model saved to {filename}.")
-
 
 def load_data(data_dir):
     """
