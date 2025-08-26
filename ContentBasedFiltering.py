16cf23bd3d7e5482de1eaa117d922502fc7ede96
Kok How, Teh <funcoolgeek@gmail.com>
2025-08-26 15:44:56 +0800

    Add description to ContentBasedFiltering class

--- ContentBasedFiltering.py
+++ ContentBasedFiltering.py
@@ -20,6 +20,19 @@ class L2_Normalize(Layer):
         return tf.linalg.l2_normalize(x, axis)
     
 class ContentBasedFiltering():
+    """
+    UnSupervised Learning.
+    Implement the collaborative filtering learning algorithm and apply it to a dataset of movie ratings.
+    The goal of a collaborative filtering recommender system is to generate two vectors: 
+    For each user, a 'parameter vector' that embodies the movie tastes of a user. 
+    For each movie, a feature vector of the same size which embodies some description of the movie. 
+    The dot product of the two vectors plus the bias term should produce an estimate of the rating the user might give to that movie.
+    Existing ratings are provided in matrix form as shown. Y contains ratings; 0.5 to 5 inclusive in 0.5 steps. 0 if the movie has not been rated. R has a 1 where movies have been rated. 
+    Movies are in rows, users in columns. Each user has a parameter vector w^{user} and bias. Each movie has a feature vector x^{movie}. These vectors are simultaneously learned by using the existing user/movie ratings as training data. For example, w^{(1)} dot x^{(1)} + b^{(1)} = 4.
+    It is worth noting that the feature vector $x^{movie} must satisfy all the users while the user vector w^{user} must satisfy all the movies. This is the source of the name of this approach - all the users collaborate to generate the rating set.
+    Once the feature vectors and parameters are learned, they can be used to predict how a user might rate an unrated movie. This is shown in the diagram above. The equation is an example of predicting a rating for user one on movie zero.
+    The function `cofiCostFunc` that computes the collaborative filtering objective function. After implementing the objective function, you will use a TensorFlow custom training loop to learn the parameters for collaborative filtering. The first step is to detail the data set and data structures that will be used in the lab.
+    """
     _item_train = None
     _item_test = None
     _user_train = None
