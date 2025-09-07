952c1adea5f944afbf932aee07068424d155fc38
Kok How, Teh <funcoolgeek@gmail.com>
2025-09-07 17:00:12 +0800

    Add UNet, update YOLO and add notes on face recognition, neural style transfer

---
+++ UNet.py
@@ -0,0 +1,165 @@
+import os, numpy, pandas as pd, imageio
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import concatenate, Input, Conv2D, MaxPooling2D, Dropout, Conv2DTranspose
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.optimizers import Adam
+
+class UNet():
+    _input_size = None
+    _n_filters: int = None
+    _n_classes: int = None
+    _model: Model = None
+    def __init__(self, input_size, n_filters:int, n_classes: int):
+        self._input_size = input_size
+        self._n_filters = n_filters
+        self._n_classes = n_classes
+    """
+    The encoder is a stack of various Encoders:
+
+    Each Encoder() is composed of 2 Conv2D layers with ReLU activations. We will apply Dropout, and MaxPooling2D to some Encoders, as you will verify in the following sections, specifically to the last two blocks of the downsampling.
+
+    The function will return two tensors:
+
+    next_layer: That will go into the next block.
+    skip_connection: That will go into the corresponding decoding block.
+    Note: If max_pooling=True, the next_layer will be the output of the MaxPooling2D layer, but the skip_connection will be the output of the previously applied layer(Conv2D or Dropout, depending on the case). Else, both results will be identical.
+    """
+    def Encoder(self, inputs=None, n_filters=32, dropout_prob=0, max_pooling=True):
+        """
+        Convolutional downsampling block
+        
+        Arguments:
+            inputs -- Input tensor
+            n_filters -- Number of filters for the convolutional layers
+            dropout_prob -- Dropout probability
+            max_pooling -- Use MaxPooling2D to reduce the spatial dimensions of the output volume
+        Returns: 
+            next_layer, skip_connection --  Next layer and skip connection outputs
+        """
+        conv = Conv2D(n_filters, # Number of filters
+                    3,   # Kernel size   
+                    activation="relu",
+                    padding="same",
+                    kernel_initializer='he_normal')(inputs)
+        conv = Conv2D(n_filters, # Number of filters
+                    3,   # Kernel size
+                    activation="relu",
+                    padding="same",
+                    # set 'kernel_initializer' same as above
+                    kernel_initializer='he_normal')(conv)
+        
+        # if dropout_prob > 0 add a dropout layer, with the variable dropout_prob as parameter
+        if dropout_prob > 0:
+            conv = Dropout(dropout_prob)(conv)
+            
+        # if max_pooling is True add a MaxPooling2D with 2x2 pool_size
+        if max_pooling:
+            next_layer = MaxPooling2D(2)(conv)
+        else:
+            next_layer = conv
+            
+        skip_connection = conv
+        return next_layer, skip_connection
+
+    def Decoder(self, expansive_input, contractive_input, n_filters=32):
+        """
+        Convolutional upsampling block
+        
+        Arguments:
+            expansive_input -- Input tensor from previous layer
+            contractive_input -- Input tensor from previous skip layer
+            n_filters -- Number of filters for the convolutional layers
+        Returns: 
+            conv -- Tensor output
+        """
+        
+        ### START CODE HERE
+        up = Conv2DTranspose(
+                    n_filters,    # number of filters
+                    3,    # Kernel size
+                    strides=(2,2),
+                    padding="same")(expansive_input)
+        
+        # Merge the previous output and the contractive_input
+        merge = concatenate([up, contractive_input], axis=3)
+        conv = Conv2D(n_filters,   # Number of filters
+                    3,     # Kernel size
+                    activation="relu",
+                    padding="same",
+                    kernel_initializer='he_normal')(merge)
+        conv = Conv2D(n_filters,  # Number of filters
+                    3,   # Kernel size
+                    activation="relu",
+                    padding="same",
+                    # set 'kernel_initializer' same as above
+                    kernel_initializer='he_normal')(conv)
+        ### END CODE HERE
+        
+        return conv
+
+    def BuildModel(self):
+        """
+        Unet model
+        
+        Arguments:
+            input_size -- Input shape 
+            n_filters -- Number of filters for the convolutional layers
+            n_classes -- Number of output classes
+        Returns: 
+            model -- tf.keras.Model
+        """
+        inputs = Input(self._input_size)
+        # Contracting Path (encoding)
+        # Add a Encoder with the inputs of the unet_ model and n_filters
+        ### START CODE HERE
+        cblock1 = self.Encoder(inputs, self._n_filters)
+        # Chain the first element, [0], of the output of each block to be the input of the next Encoder. 
+        # Double the number of filters at each new step
+        cblock2 = self.Encoder(cblock1[0], self._n_filters * 2)
+        cblock3 = self.Encoder(cblock2[0], self._n_filters * 4)
+        cblock4 = self.Encoder(cblock3[0], self._n_filters * 8, dropout_prob=0.3) # Include a dropout_prob of 0.3 for this layer
+        # Include a dropout_prob of 0.3 for this layer, and avoid the max_pooling layer
+        cblock5 = self.Encoder(cblock4[0], self._n_filters * 16, dropout_prob=0.3, max_pooling=None) 
+        ### END CODE HERE
+        
+        # Expanding Path (decoding)
+        # Add the first Decoder.
+        # Use the cblock5[0] as expansive_input and cblock4[1] as contractive_input and n_filters * 8
+        ### START CODE HERE
+        ublock6 = self.Decoder(cblock5[0], cblock4[1],  self._n_filters * 8)
+        # Chain the output of the previous block as expansive_input and the corresponding contractive block output.
+        # Note that you must use the second element, [1], of the contractive block i.e before the maxpooling layer. 
+        # At each step, use half the number of filters of the previous block 
+        ublock7 = self.Decoder(ublock6, cblock3[1],  self._n_filters * 4)
+        ublock8 = self.Decoder(ublock7, cblock2[1],  self._n_filters * 2)
+        ublock9 = self.Decoder(ublock8, cblock1[1],  self._n_filters)
+        ### END CODE HERE
+
+        conv9 = Conv2D(self._n_filters,
+                    3,
+                    activation='relu',
+                    padding='same',
+                    # set 'kernel_initializer' same as above exercises
+                    kernel_initializer='he_normal')(ublock9)
+
+        # Add a Conv2D layer with n_classes filter, kernel size of 1 and a 'same' padding
+        ### START CODE HERE
+        conv10 = Conv2D(self._n_classes, 1, padding="same")(conv9)
+        ### END CODE HERE
+        
+        self._model = tf.keras.Model(inputs=inputs, outputs=conv10)
+        # In semantic segmentation, you need as many masks as you have object classes. In the dataset you're using, each pixel in every mask has been assigned a single integer probability that it belongs to a certain class, from 0 to num_classes-1. The correct class is the layer with the higher probability.
+        # This is different from categorical crossentropy, where the labels should be one-hot encoded (just 0s and 1s). Here, you'll use sparse categorical crossentropy as your loss function, to perform pixel-wise multiclass prediction. Sparse categorical crossentropy is more efficient than other loss functions when you're dealing with lots of classes.
+        self._model.compile(optimizer=Adam(0.01),
+                    loss=SparseCategoricalCrossentropy(from_logits=True),
+                    metrics=['accuracy'])
+        self._model.summary()
+
+if __name__ == "__main__":
+    img_height = 96
+    img_width = 128
+    num_channels = 3
+    unet = UNet((img_height, img_width, num_channels), 32, 23)
+    unet.BuildModel()
--- YOLO.py
+++ YOLO.py
@@ -12,8 +12,40 @@ from tensorflow.keras.models import load_model
 #from yad2k.models.keras_yolo import yolo_head
 #from yad2k.utils.utils import draw_boxes, get_colors_for_classes, scale_boxes, read_classes, read_anchors, preprocess_image
 
-# UNQ_C1 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-# GRADED FUNCTION: yolo_filter_boxes
+"""
+Compute box scores by doing the elementwise product as described in Figure 4 ( 𝑝×𝑐 ).
+The following code may help you choose the right operator:
+
+a = numpy.random.randn(19, 19, 5, 1)
+b = numpy.random.randn(19, 19, 5, 80)
+c = a * b # shape of c will be (19, 19, 5, 80)
+This is an example of broadcasting (multiplying vectors of different sizes).
+
+For each box, find:
+
+the index of the class with the maximum box score
+
+the corresponding box score
+
+Useful References
+
+tf.math.argmax
+tf.math.reduce_max
+Helpful Hints
+
+For the axis parameter of argmax and reduce_max, if you want to select the last axis, one way to do so is to set axis=-1. This is similar to Python array indexing, where you can select the last position of an array using arrayname[-1].
+Applying reduce_max normally collapses the axis for which the maximum is applied. keepdims=False is the default option, and allows that dimension to be removed. You don't need to keep the last dimension after applying the maximum here.
+Create a mask by using a threshold. As a reminder: ([0.9, 0.3, 0.4, 0.5, 0.1] < 0.4) returns: [False, True, False, False, True]. The mask should be True for the boxes you want to keep.
+
+Use TensorFlow to apply the mask to box_class_scores, boxes and box_classes to filter out the boxes you don't want. You should be left with just the subset of boxes you want to keep.
+
+One more useful reference:
+
+tf.boolean mask
+And one more helpful hint: :)
+
+For the tf.boolean_mask, you can keep the default axis=None.
+"""
 def yolo_filter_boxes(boxes, box_confidence, box_class_probs, threshold = .6):
     print(f"\n=== {yolo_filter_boxes.__name__} ===")
     """Filters YOLO boxes by thresholding on object and class confidence.
@@ -103,6 +135,195 @@ def IoU(box1, box2):
     ### END CODE HERE
     
     return iou
+"""
+The key steps are as follows:
+
+Select the box with the highest score.
+Compute the overlap of this box with all other boxes of the same class. Remove any boxes that significantly overlap (IoU >= iou_threshold).
+Repeat the process: go back to step 1 and iterate until no remaining boxes have a score lower than the currently selected box.
+This process effectively eliminates boxes that overlap significantly with the selected boxes, leaving only the "best" candidates.
+
+For implementation, consider using a divide-and-conquer approach:
+
+Divide all the boxes by class.
+Apply non-max suppression for each class individually.
+Finally, merge the results from each class into a single set of boxes.
+This approach is recommended for the upcoming exercise, as it simplifies the implementation while ensuring accuracy across multiple classes.
+
+
+Exercise 3 - yolo_non_max_suppression
+Implement yolo_non_max_suppression() using TensorFlow. TensorFlow has two built-in functions that are used to implement non-max suppression (so you don't actually need to use your iou() implementation):
+
+Reference documentation:
+
+tf.image.non_max_suppression()
+tf.image.non_max_suppression(
+  boxes, scores, max_output_size, iou_threshold=0.5,
+  score_threshold=float('-inf'), name=None
+)
+Note: In the exercise below, for tf.image.non_max_suppression() you only need to set boxes, scores, max_output_size and iou_threshold parameters.
+tf.gather()
+tf.gather(
+  params, indices, validate_indices=None, axis=None, batch_dims=0, name=None
+)
+Note: In the exercise below, for tf.gather() you only need to set params and indices parameters.
+
+tf.boolean mask()
+tf.boolean_mask(
+  tensor, mask, axis=None, name='boolean_mask'
+)
+Note: In the exercise below, for tf.boolean_mask() you only need to set tensor and mask parameters.
+
+tf.concat()
+tf.concat(
+  values, axis, name='concat'
+)
+"""
+def yolo_non_max_suppression(scores, boxes, classes, max_boxes = 10, iou_threshold = 0.5):
+    """
+    Applies Non-max suppression (NMS) to set of boxes
+    
+    Arguments:
+    scores -- tensor of shape (None,), output of yolo_filter_boxes()
+    boxes -- tensor of shape (None, 4), output of yolo_filter_boxes() that have been scaled to the image size (see later)
+    classes -- tensor of shape (None,), output of yolo_filter_boxes()
+    max_boxes -- integer, maximum number of predicted boxes you'd like
+    iou_threshold -- real value, "intersection over union" threshold used for NMS filtering
+    
+    Returns:
+    scores -- tensor of shape (None, ), predicted score for each box
+    boxes -- tensor of shape (None, 4), predicted box coordinates
+    classes -- tensor of shape (None, ), predicted class for each box
+    
+    Note: The "None" dimension of the output tensors has obviously to be less than max_boxes. Note also that this
+    function will transpose the shapes of scores, boxes, classes. This is made for convenience.
+    """
+    print(f"\n=== {yolo_non_max_suppression.__name__} ===")
+    boxes = tf.cast(boxes, dtype=tf.float32)
+    scores = tf.cast(scores, dtype=tf.float32)
+    nms_indices = []
+    classes_labels = tf.unique(classes)[0] # Get unique classes
+    print(f"boxes: {boxes.shape}, scores: {scores.shape}, classes: {classes}, classes_labels: {classes_labels}")
+
+    for label in classes_labels:
+        print(f"Process label: {label}...")
+        filtering_mask = classes == label
+        print(f"filtering_mask: {filtering_mask}")
+    
+        # Get boxes for this class
+        # Use tf.boolean_mask() with 'boxes' and `filtering_mask`
+        boxes_label = tf.boolean_mask(boxes, filtering_mask)
+        
+        # Get scores for this class
+        # Use tf.boolean_mask() with 'scores' and `filtering_mask`
+        scores_label = tf.boolean_mask(scores, filtering_mask)
+        
+        if tf.shape(scores_label)[0] > 0:  # Check if there are any boxes to process
+            
+            # Use tf.image.non_max_suppression() to get the list of indices corresponding to boxes you keep
+            ##(≈ 5 lines)
+            nms_indices_label = tf.image.non_max_suppression(
+                    boxes_label,
+                    scores_label,
+                    max_boxes,
+                    iou_threshold=iou_threshold) 
+
+            # Get original indices of the selected boxes
+            selected_indices = tf.squeeze(tf.where(filtering_mask), axis=1)
+            
+            # Append the resulting boxes into the partial result
+            # Use tf.gather() with 'selected_indices' and `nms_indices_label`
+            nms_indices.append(tf.gather(selected_indices, nms_indices_label))
+
+    # Flatten the list of indices and concatenate
+    # Use tf.concat() with 'nms_indices' and `axis=0`
+    nms_indices = tf.concat(nms_indices, axis=0)
+    
+    # Use tf.gather() to select only nms_indices from scores, boxes and classes
+    ##(≈ 3 lines)
+    scores = tf.gather(scores, nms_indices)
+    boxes = tf.gather(boxes, nms_indices)
+    classes = tf.gather(classes, nms_indices)
+    
+    # Sort by scores and return the top max_boxes
+    sort_order = tf.argsort(scores, direction='DESCENDING').numpy()
+    scores = tf.gather(scores, sort_order[0:max_boxes])
+    boxes = tf.gather(boxes, sort_order[0:max_boxes])
+    classes = tf.gather(classes, sort_order[0:max_boxes])
+
+    return scores, boxes, classes
+
+def yolo_boxes_to_corners(box_xy, box_wh):
+    """Convert YOLO box predictions to bounding box corners."""
+    box_mins = box_xy - (box_wh / 2.)
+    box_maxes = box_xy + (box_wh / 2.)
+
+    return tf.keras.backend.concatenate([
+        box_mins[..., 1:2],  # y_min
+        box_mins[..., 0:1],  # x_min
+        box_maxes[..., 1:2],  # y_max
+        box_maxes[..., 0:1]  # x_max
+    ])
+"""
+takes the output of the YOLO encoding and filters the boxes using score threshold and NMS. There's just one last implementational detail you have to know. There're a few ways of representing boxes, such as via their corners or via their midpoint and height/width. YOLO converts between a few such formats at different times, using the following functions (which are provided):
+
+boxes = yolo_boxes_to_corners(box_xy, box_wh) 
+which converts the yolo box coordinates (x,y,w,h) to box corners' coordinates (x1, y1, x2, y2) to fit the input of yolo_filter_boxes
+
+boxes = scale_boxes(boxes, image_shape)
+YOLO's network was trained to run on 608x608 images. If you are testing this data on a different size image -- for example, the car detection dataset had 720x1280 images -- this step rescales the boxes so that they can be plotted on top of the original 720x1280 image.
+"""
+def yolo_eval(yolo_outputs, image_shape = (720, 1280), max_boxes=10, score_threshold=.6, iou_threshold=.5):
+    """
+    Converts the output of YOLO encoding (a lot of boxes) to your predicted boxes along with their scores, box coordinates and classes.
+    
+    Arguments:
+    yolo_outputs -- output of the encoding model (for image_shape of (608, 608, 3)), contains 4 tensors:
+                    box_xy: tensor of shape (None, 19, 19, 5, 2)
+                    box_wh: tensor of shape (None, 19, 19, 5, 2)
+                    box_confidence: tensor of shape (None, 19, 19, 5, 1)
+                    box_class_probs: tensor of shape (None, 19, 19, 5, 80)
+    image_shape -- tensor of shape (2,) containing the input shape, in this notebook we use (608., 608.) (has to be float32 dtype)
+    max_boxes -- integer, maximum number of predicted boxes you'd like
+    score_threshold -- real value, if [ highest class probability score < threshold], then get rid of the corresponding box
+    iou_threshold -- real value, "intersection over union" threshold used for NMS filtering
+    
+    Returns:
+    scores -- tensor of shape (None, ), predicted score for each box
+    boxes -- tensor of shape (None, 4), predicted box coordinates
+    classes -- tensor of shape (None,), predicted class for each box
+    """
+    
+    ### START CODE HERE
+    
+    # Retrieve outputs of the YOLO model (≈1 line)
+    box_xy, box_wh, box_confidence, box_class_probs = yolo_outputs
+    
+    # Convert boxes to be ready for filtering functions (convert boxes box_xy and box_wh to corner coordinates)
+    boxes = yolo_boxes_to_corners(box_xy, box_wh)
+    
+    # Use the function `yolo_filter_boxes` you've implemented to perform Score-filtering with a threshold of score_threshold
+    scores, boxes, classes = yolo_filter_boxes(boxes, # Use boxes
+                                  box_confidence, # Use box confidence
+                                  box_class_probs, # Use box class probability
+                                  score_threshold  # Use threshold=score_threshold
+                                 )
+    
+    # Scale boxes back to original image shape.
+    boxes = scale_boxes(boxes, image_shape)
+    
+    # Use the function `yolo_non_max_suppression` you've implemented to perform Non-max suppression with 
+    # maximum number of boxes set to max_boxes and a threshold of iou_threshold
+    scores, boxes, classes = yolo_non_max_suppression(scores, # Use scores
+                                  boxes, # Use boxes
+                                  classes, # Use classes
+                                  max_boxes, # Use max boxes
+                                  iou_threshold  # Use iou_threshold=iou_threshold
+                                 )
+    
+    ### END CODE HERE
+    
+    return scores, boxes, classes
 
 def yolo_filter_boxes_test():
     box_confidence = tf.random.normal([19, 19, 5, 1], mean=1, stddev=4, seed = 1)
@@ -142,7 +363,74 @@ def IoUTest():
     box2 = (2,3,3,4)
     print("iou for boxes that only touch at edges = " + str(IoU(box1,box2)))
     assert IoU(box1, box2) == 0, "Intersection at edges must be 0"    
-    
+
+def yolo_non_max_suppression_test():
+    scores = numpy.array([0.855, 0.828])
+    boxes = numpy.array([[0.45, 0.2,  1.01, 2.6], [0.42, 0.15, 1.7, 1.01]])
+    classes = numpy.array([0, 1])
+
+    print(f"iou:    \t{IoU(boxes[0], boxes[1])}")
+
+    scores2, boxes2, classes2 = yolo_non_max_suppression(scores, boxes, classes, iou_threshold = 0.9)
+
+    assert numpy.allclose(scores2.numpy(), [0.855, 0.828]), f"Wrong value on scores {scores2.numpy()}"
+    assert numpy.allclose(boxes2.numpy(), [[0.45, 0.2,  1.01, 2.6], [0.42, 0.15, 1.7, 1.01]]), f"Wrong value on boxes {boxes2.numpy()}"
+    assert numpy.array_equal(classes2.numpy(), [0, 1]), f"Wrong value on classes {classes2.numpy()}"
+
+    scores2, boxes2, classes2 = yolo_non_max_suppression(scores, boxes, classes, iou_threshold = 0.1)
+
+    assert numpy.allclose(scores2.numpy(), [0.855, 0.828]), f"Wrong value on scores {scores2.numpy()}"
+    assert numpy.allclose(boxes2.numpy(), [[0.45, 0.2,  1.01, 2.6], [0.42, 0.15, 1.7, 1.01]]), f"Wrong value on boxes {boxes2.numpy()}"
+    assert numpy.array_equal(classes2.numpy(), [0, 1]), f"Wrong value on classes {classes2.numpy()}"
+
+    classes = numpy.array([0, 0])
+
+    # If both boxes belongs to the same class, one box is suppressed if iou is under the iou_threshold
+    scores2, boxes2, classes2 = yolo_non_max_suppression(scores, boxes, classes, iou_threshold = 0.15)
+
+    assert numpy.allclose(scores2.numpy(), [0.855]), f"Wrong value on scores {scores2.numpy()}"
+    assert numpy.allclose(boxes2.numpy(), [[0.45, 0.2,  1.01, 2.6]]), f"Wrong value on boxes {boxes2.numpy()}"
+    assert numpy.array_equal(classes2.numpy(), [0]), f"Wrong value on classes {classes2.numpy()}"
+
+    # It must return both boxes, as they belong to different classes
+    print(f"scores:  \t{scores2.numpy()}")
+    print(f"boxes:  \t{boxes2.numpy()}")     
+    print(f"classes:\t{classes2.numpy()}")
+
+    # If both boxes belongs to the same class, one box is suppressed if iou is under the iou_threshold
+    scores2, boxes2, classes2 = yolo_non_max_suppression(scores, boxes, [0, 0], iou_threshold = 0.9)
+
+    assert numpy.allclose(scores2.numpy(), [0.855, 0.828]), f"Wrong value on scores {scores2.numpy()}"
+    assert numpy.allclose(boxes2.numpy(), [[0.45, 0.2,  1.01, 2.6], [0.42, 0.15, 1.7, 1.01]]), f"Wrong value on boxes {boxes2.numpy()}"
+    assert numpy.array_equal(classes2.numpy(), [0, 0]), f"Wrong value on classes {classes2.numpy()}"
+
+def yolo_eval_test():
+    yolo_outputs = (tf.random.normal([19, 19, 5, 2], mean=1, stddev=4, seed = 1),
+                    tf.random.normal([19, 19, 5, 2], mean=1, stddev=4, seed = 1),
+                    tf.random.normal([19, 19, 5, 1], mean=1, stddev=4, seed = 1),
+                    tf.random.normal([19, 19, 5, 80], mean=1, stddev=4, seed = 1))
+    scores, boxes, classes = yolo_eval(yolo_outputs)
+    print("scores[2] = " + str(scores[2].numpy()))
+    print("boxes[2] = " + str(boxes[2].numpy()))
+    print("classes[2] = " + str(classes[2].numpy()))
+    print("scores.shape = " + str(scores.numpy().shape))
+    print("boxes.shape = " + str(boxes.numpy().shape))
+    print("classes.shape = " + str(classes.numpy().shape))
+
+    assert type(scores) == EagerTensor, "Use tensoflow functions"
+    assert type(boxes) == EagerTensor, "Use tensoflow functions"
+    assert type(classes) == EagerTensor, "Use tensoflow functions"
+
+    assert scores.shape == (10,), "Wrong shape"
+    assert boxes.shape == (10, 4), "Wrong shape"
+    assert classes.shape == (10,), "Wrong shape"
+        
+    assert numpy.isclose(scores[2].numpy(), 171.60194), "Wrong value on scores"
+    assert numpy.allclose(boxes[2].numpy(), [-1240.3483, -3212.5881, -645.78, 2024.3052]), "Wrong value on boxes"
+    assert numpy.isclose(classes[2].numpy(), 16), "Wrong value on classes"    
+
 if __name__ == "__main__":
     yolo_filter_boxes_test()
     IoUTest()
+    yolo_non_max_suppression_test()
+    yolo_eval_test()
