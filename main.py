f2fd0ad76d0eedcd23986012128a5391dfa803a4
Kok How, Teh <funcoolgeek@gmail.com>
2025-10-12 13:01:04 +0800

    Add sring manipulation using list comprehension and min edit distance DP

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy, pandas as pd
 from pathlib import Path
 from utils.UniVariatePlot import UniVariatePlot
 from utils.CosineSimilarity import cosine_similarity
@@ -127,6 +127,142 @@ def CountSubStrings(src, substr, repetition):
     src *= repetition
     print(f"'{substr}' appears in `{src}` {src.count(substr)} times")
 
+def DeleteCharFromString(word: str):
+    '''
+    Input:
+        word: the string/word for which you will generate all possible words 
+                in the vocabulary which have 1 missing character
+    Output:
+        delete_l: a list of all possible strings obtained by deleting 1 character from word
+    '''    
+    print(f"\n=== {DeleteCharFromString.__name__} ===")
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    result = [L + R[1:] for L, R in splits if R] # This always removes the first character, [0] from the second concatenated string, R
+    print(f"result: {result}")
+
+def InsertCharString(word: str):
+    '''
+    Input:
+        word: the input string/word 
+    Output:
+        inserts: a set of all possible strings with one new letter inserted at every offset
+    ''' 
+    print(f"\n=== {InsertCharString.__name__} ===")
+    chars = 'abcdefghijklmnopqrstuvwxyz'
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    result = []
+    for L, R in splits: # This always replaces the first character, [0] from the second concatenated string, R
+        for c in chars:
+            result.append(L + c + R)
+    print(f"result: {len(result)} {result}")
+
+def SwitchLetters(word: str):
+    '''
+    Input:
+        word: input string
+     Output:
+        switches: a list of all possible strings with one adjacent charater switched
+    '''
+    print(f"\n=== {SwitchLetters.__name__} ===")
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    """
+    ince, ncie, niec
+    [('', 'nice'), ('n', 'ice), ('ni', 'ce'), ('nic', 'e'), 'nice', '')]
+    ('', 'nice') : 'ince'
+    ('n', 'ice) : 'n' + 'cie'
+    ('ni', 'ce') : 'ni' + 'ec'
+    """
+    result = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) >= 2]
+    print(f"result: {result}")
+
+def ReplaceCharsPermutation(word: str):
+    '''
+    Input:
+        word: the input string/word 
+    Output:
+        replaces: a list of all possible strings where we replaced one letter from the original word. 
+    '''
+    print(f"\n=== {ReplaceCharsPermutation.__name__} ===")
+    chars = 'abcdefghijklmnopqrstuvwxyz'
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    result = set()
+    for L, R in splits: # This always replaces the first character, [0] from the second concatenated string, R
+        if R:
+            for c in chars:
+                tmp = L + c + R[1:]
+                if tmp != word:
+                    result.add(tmp)
+    print(f"result: {len(result)} {result}")
+    return result
+
+def min_edit_distance(source, target, ins_cost = 1, del_cost = 1, rep_cost = 2):
+    '''
+    Input: 
+        source: a string corresponding to the string you are starting with
+        target: a string corresponding to the string you want to end with
+        ins_cost: an integer setting the insert cost
+        del_cost: an integer setting the delete cost
+        rep_cost: an integer setting the replace cost
+    Output:
+        D: a matrix of len(source)+1 by len(target)+1 containing minimum edit distances
+        med: the minimum edit distance (med) required to convert the source string to the target
+    '''
+    print(f"\n=== {min_edit_distance.__name__} ===")
+    # use deletion and insert cost as  1
+    m = len(source) 
+    n = len(target) 
+    #initialize cost matrix with zeros and dimensions (m+1,n+1) 
+    D = numpy.zeros((m+1, n+1), dtype=int) 
+    
+    # Fill in column 0, from row 1 to row m, both inclusive
+    for row in range(1, m + 1): # Replace None with the proper range
+        D[row,0] = D[row-1,0] + del_cost
+        
+    # Fill in row 0, for all columns from 1 to n, both inclusive
+    for col in range(1, n + 1): # Replace None with the proper range
+        D[0,col] = D[0, col-1] + ins_cost
+        
+    # Loop through row 1 to row m, both inclusive
+    for i in range(1,m+1):
+        
+        # Loop through column 1 to column n, both inclusive
+        for j in range(1,n+1):
+            
+            # Intialize r_cost to the 'replace' cost that is passed into this function
+            r_cost = 0 if source[i-1] == target[j-1] else rep_cost
+            
+            # Update the cost at row, col based on previous entries in the cost matrix
+            # Refer to the equation calculate for D[i,j] (the minimum of three calculated costs)
+            D[i,j] = min(D[i-1,j] + del_cost, D[i,j-1] + ins_cost, D[i-1,j-1] + r_cost)
+            
+    # Set the minimum edit distance with the cost found at row m, column n 
+    med = D[m,n]
+    return D, med
+
+def min_edit_distance_tests():
+    print(f"\n=== {min_edit_distance_tests.__name__} ===")
+    source =  'play'
+    target = 'stay'
+    matrix, min_edits = min_edit_distance(source, target)
+    assert 4 == min_edits
+    print("minimum edits: ",min_edits, "\n")
+    idx = list('#' + source)
+    cols = list('#' + target)
+    df = pd.DataFrame(matrix, index=idx, columns= cols)
+    print(df)
+
+    source =  'eer'
+    target = 'near'
+    matrix, min_edits = min_edit_distance(source, target)
+    assert 3 == min_edits
+    print("minimum edits: ",min_edits, "\n")
+    idx = list(source)
+    idx.insert(0, '#')
+    cols = list(target)
+    cols.insert(0, '#')
+    df = pd.DataFrame(matrix, index=idx, columns= cols)
+    print(df)
+
 def CosineSimilarityTests():
     print(f"\n=== {CosineSimilarityTests.__name__} ===")
     usa = numpy.array([5,6])
@@ -153,4 +289,11 @@ if __name__ == "__main__":
     TupleArithmetic()
     ArrayExtension()
     CountSubStrings("banana", "an", 4)
+    DeleteCharFromString("banana")
+    InsertCharString("at")
+    SwitchLetters("nice")
+    strings = set(['aan', 'ban', 'caa', 'cab', 'cac', 'cad', 'cae', 'caf', 'cag', 'cah', 'cai', 'caj', 'cak', 'cal', 'cam', 'cao', 'cap', 'caq', 'car', 'cas', 'cat', 'cau', 'cav', 'caw', 'cax', 'cay', 'caz', 'cbn', 'ccn', 'cdn', 'cen', 'cfn', 'cgn', 'chn', 'cin', 'cjn', 'ckn', 'cln', 'cmn', 'cnn', 'con', 'cpn', 'cqn', 'crn', 'csn', 'ctn', 'cun', 'cvn', 'cwn', 'cxn', 'cyn', 'czn', 'dan', 'ean', 'fan', 'gan', 'han', 'ian', 'jan', 'kan', 'lan', 'man', 'nan', 'oan', 'pan', 'qan', 'ran', 'san', 'tan', 'uan', 'van', 'wan', 'xan', 'yan', 'zan'])
+    result = ReplaceCharsPermutation("can")
+    print(f"\nstrings: {len(strings)}, difference: {result - strings}")
+    min_edit_distance_tests()
     CosineSimilarityTests()
