7252e4985838ba805b85a2b0b45d4f677b1ee160
Kok How, Teh <funcoolgeek@gmail.com>
2025-11-02 14:08:10 +0800

    Refactor SmilingFace binary classifier to combin build and train model

--- SmilingFaceConvNet.py
+++ SmilingFaceConvNet.py
@@ -1,8 +1,11 @@
 import argparse, numpy, h5py
-from pathlib import Path
 import tensorflow as tf
+from pathlib import Path
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.layers import Input, ZeroPadding2D, Conv2D, BatchNormalization, ReLU, MaxPooling2D, Dense, Flatten
 from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy
-from tensorflow.keras import layers, losses, optimizers, regularizers
+from tensorflow.keras.models import load_model
+from tensorflow.keras import layers, losses, optimizers, regularizers, Sequential
 from tensorflow.keras.optimizers import Adam
 from utils.GPU import InitializeGPU
 from utils.TrainingMetricsPlot import PlotModelHistory
@@ -17,17 +20,19 @@ class SmilingFaceConvNet():
     _classes: numpy.array = None
     _model: tf.keras.Sequential = None
     _model_path: str = None
-    _trained: bool = False
-    def __init__(self, path:str):
+    _learning_rate:float = None
+    _batch_size:int = None
+    def __init__(self, path:str, learning_rate:float, batch_size:int):
         InitializeGPU()
+        self._learning_rate = learning_rate
+        self._batch_size = batch_size
         self._model_path = path
-        self._prepare_data()
+        self._PrepareData()
         if self._model_path and len(self._model_path) and Path(self._model_path).exists() and Path(self._model_path).is_file():
             print(f"Using saved model {self._model_path}...")
-            self._model = tf.keras.models.load_model(self._model_path)
-            self._trained = True
+            self._model = load_model(self._model_path)
 
-    def _prepare_data(self):
+    def _PrepareData(self):
         train_dataset = h5py.File('data/train_happy.h5', "r")
         self._X_train = numpy.array(train_dataset["train_set_x"][:]) # your train set features
         self._Y_train = numpy.array(train_dataset["train_set_y"][:]) # your train set labels
@@ -56,7 +61,7 @@ class SmilingFaceConvNet():
         print ("Y_test shape: " + str(self._Y_test.shape))
         print(f"Class#: {self._classes}")
 
-    def BuildModel(self, rebuild:bool = False, learning_rate: float = 0.01):
+    def BuildTrainModel(self, retrain:bool, epochs:int):
         """
         Implements the forward propagation for the binary classification model:
         ZEROPAD2D -> CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> FLATTEN -> DENSE
@@ -71,45 +76,48 @@ class SmilingFaceConvNet():
         Returns:
         model -- TF Keras model (object containing the information for the entire training process) 
         """
-        if self._model and not rebuild:
-            return
-        self._model = tf.keras.Sequential([
-                ## ZeroPadding2D with padding 3, input shape of 64 x 64 x 3
-                layers.Input(shape=(64,64,3)),
-                layers.ZeroPadding2D(padding=3),
-                ## Conv2D with 32 7x7 filters and stride of 1
-                layers.Conv2D(32, (7, 7), strides=(1, 1)),
-                ## BatchNormalization for axis 3
-                layers.BatchNormalization(axis=3), # stabilize the learning process, accelerate convergence (speed up training), and potentially improve generalization performance.
-                ## ReLU
-                layers.ReLU(),
-                ## Max Pooling 2D with default parameters
-                layers.MaxPooling2D(),
-                ## Flatten layer
-                layers.Flatten(),
-                ## Dense layer with 1 unit for output & 'sigmoid' activation
-                layers.Dense(1)   # Linear activation ("pass-through") if not specified
-            ])
-        self._model.compile(
-                loss=BinaryCrossentropy(from_logits=True), # Logistic Loss: -ylog(f(X)) - (1 - y)log(1 - f(X)) Defaults to sigmoid activation which is typically used for binary classification
-                optimizer=Adam(learning_rate=learning_rate), # Intelligent gradient descent which automatically adjusts the learning rate (alpha) depending on the direction of the gradient descent.
-                metrics=['accuracy']
-            )
-        self._model.summary()
-
-    def TrainEvaluate(self, rebuild:bool, epochs:int, batch_size:int):
-        if self._model:
-            if not self._trained or rebuild:
-                history = self._model.fit(self._X_train, self._Y_train, epochs=epochs, batch_size=batch_size)
-                print(f"history: {history.history}")
-                PlotModelHistory("Smiling face binary classifier", history)
-                self._trained = True
-                if self._model_path:
-                    self._model.save(self._model_path)
-                    print(f"Model saved to {self._model_path}.")
+        new_model = not self._model
+        if new_model:
+            self._model = Sequential([
+                    ## ZeroPadding2D with padding 3, input shape of 64 x 64 x 3
+                    Input(shape=(64,64,3)),
+                    ZeroPadding2D(padding=3),
+                    ## Conv2D with 32 7x7 filters and stride of 1
+                    Conv2D(32, (7, 7), strides=(1, 1)),
+                    ## BatchNormalization for axis 3
+                    BatchNormalization(axis=3), # stabilize the learning process, accelerate convergence (speed up training), and potentially improve generalization performance.
+                    ## ReLU
+                    ReLU(),
+                    ## Max Pooling 2D with default parameters
+                    MaxPooling2D(),
+                    ## Flatten layer
+                    Flatten(),
+                    ## Dense layer with 1 unit for output & 'sigmoid' activation
+                    Dense(1)   # Linear activation ("pass-through") if not specified
+                ])
+            self._model.compile(
+                    loss=BinaryCrossentropy(from_logits=True), # Logistic Loss: -ylog(f(X)) - (1 - y)log(1 - f(X)) Defaults to sigmoid activation which is typically used for binary classification
+                    optimizer=Adam(learning_rate=self._learning_rate), # Intelligent gradient descent which automatically adjusts the learning rate (alpha) depending on the direction of the gradient descent.
+                    metrics=['accuracy']
+                )
+            self._model.summary()
+            plot_model(
+                self._model,
+                to_file="output/SmilingFaceClassifier.png",
+                show_shapes=True,
+                show_dtype=True,
+                show_layer_names=True,
+                rankdir="TB",
+                expand_nested=True,
+                show_layer_activations=True)
+        if new_model or retrain:
+            history = self._model.fit(self._X_train, self._Y_train, epochs=epochs, batch_size=self._batch_size, shuffle=True)
+            print(f"history: {history.history}")
+            PlotModelHistory("Smiling face binary classifier", history)
+            if self._model_path:
+                self._model.save(self._model_path)
+                print(f"Model saved to {self._model_path}.")
             self._model.evaluate(self._X_test, self._Y_test)
-        else:
-            raise RuntimeError("Please build the model first by calling BuildModel()!")
 
 if __name__ == "__main__":
     """
@@ -120,6 +128,5 @@ if __name__ == "__main__":
     parser.add_argument('-r', '--retrain', action='store_true', help='Retrain the model')
     args = parser.parse_args()
 
-    smilingFace = SmilingFaceConvNet("models/SmilingFaceClassifier.keras")
-    smilingFace.BuildModel(args.retrain, 0.01)
-    smilingFace.TrainEvaluate(args.retrain, 10, 16)
+    smilingFace = SmilingFaceConvNet("models/SmilingFaceClassifier.keras", 0.01, 32)
+    smilingFace.BuildTrainModel(args.retrain, 30)
